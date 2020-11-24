1eb43d6bd3f4546dcef4a7939a5cf44d55c6ef1d
DanBourdier <danbourdier@aol.com>
2020-11-24 02:08:05 -0500

    added directives for classes

--- src/gameComponents/card.js
+++ src/gameComponents/card.js
@@ -1,3 +1,9 @@
+// Directives:
+//  1. construct card instances
+//  2. instance method to track visibility
+//  3. method to retrieve value
+//  4. method to change card status hidden/visible
+//  5. 
 
 class Card {
   constructor(value) {
--- src/gameComponents/game.js
+++ src/gameComponents/game.js
@@ -1,3 +1,12 @@
+// Directives:
+//  1. method to start game
+//  2. method to end game
+//  3. method to compare two cards
+//  4. track revealed cards
+//  5. track number of moves
+//  6. reset game
+//  7. track when 2/2 cards are toggled
+
 
 class Game {
   constructor() {
