970dba735e71206917b971bb6de6579a8c5ce06e
DanBourdier <danbourdier@aol.com>
2020-11-24 02:29:37 -0500

    developed logic to render cards in layout

--- src/gameComponents/layout.js
+++ src/gameComponents/layout.js
@@ -9,8 +9,24 @@ class Layout {
   }
 
   static renderLayout() {
-    const root = document.getElementById('root')
+    // First we want to target our root to hook into
+    const container = document.getElementById('root')
+    // We instantiate HTML to have a way of adding HTML each iteration of #cards
+    let html = ''
     
+    this.cards.forEach(card => {
+      let segment = `
+        <article>
+          <h3> 
+            ${ card.value }
+          </h3>
+        </article>
+      `
+      html += segment
+    })
+
+    // Finally we change the HTML in our root to contain all our cards
+    container.innerHTML = html
   }
 
 
