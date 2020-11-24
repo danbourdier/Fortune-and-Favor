e70dd90d2788d76f002884c2109e5b2cf1b114f6
DanBourdier <danbourdier@aol.com>
2020-11-24 18:38:43 -0500

    added visual styling

--- src/script.js
+++ src/script.js
@@ -1,9 +1,6 @@
-// import Card from './gameComponents/card.js'
 import Game from './gameComponents/game.js'
-// import Layout from './gameComponents/layout.js'
 
-
-// const newLayout = new Layout(3, 3)
-// newLayout.renderLayout()
 const game = new Game()
 game.startGame()
+
+console.log('Have Fun!!!')
