a85a015c21ff41e93f1a8a670ec99a6411fa305b
DanBourdier <danbourdier@aol.com>
2020-11-24 02:45:14 -0500

    added function to create n cards on execution

--- src/gameComponents/layout.js
+++ src/gameComponents/layout.js
@@ -2,11 +2,29 @@
 //  1. class method to render initial layout with inner html. Receives n*n cards
 //  2. 
 
+import Card from './card'
+
+// To externalize the ability to create multiple instances of cards for our
+//  Layout class, we accept a size argument to determine how many instaces we create
+
+const createDeck = size => {
+  let deck = []
+
+  while (deck.length < size) {
+    let card = new Card()
+    deck.push( card )
+  }
+
+  return deck
+}
+
+
 class Layout {
   constructor(rows, columns, cards) {
     this.size = Number(rows) * Number(columns)
     this.cards = cards
   }
+
   // We create a class method unavailable to child objects
   static renderLayout() {
     // First we want to target our root to hook into
@@ -28,6 +46,13 @@ class Layout {
     // Finally we change the HTML in our root to contain all our cards
     container.innerHTML = html
   }
+
+
+
+
+
+
+
 
 
 }
