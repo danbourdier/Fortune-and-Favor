d595481cc01858ffc0a761703af7e88c472dbd24
DanBourdier <danbourdier@aol.com>
2020-11-24 17:52:48 -0500

    added notes

--- src/gameComponents/card.js
+++ src/gameComponents/card.js
@@ -4,10 +4,12 @@
 //  3. method to retrieve value 
 //  4. method to change card status hidden/visible 
 //  5. assign an image to each instance
+//  6. 
 class Card {
   constructor(value) {
     this.value = value
     this.visible = false
+    
   }
 
   value() {
--- src/gameComponents/game.js
+++ src/gameComponents/game.js
@@ -8,6 +8,18 @@
 //  7. track when 2/2 cards are toggled
 
 
+// Have a base state, loading page at a loaded game
+// Restart state on click
+// Win or lose condition, if winner display etc.
+
+// OPTIONS FOR FLOW
+//  Hard refresh, 
+//  deleting instance of game,   overkill
+//  restetting instance variables by wiping layout
+  // reset moves, reset remaining Cards, in #playGame create the deck instead of passing as arg
+  //  essentially resetting all instance variables
+
+
 class Game {
   constructor(deck) {
     this.moves = 0
@@ -16,17 +28,25 @@ class Game {
     this.revealedCards = []
   }
 
-  playGame() {
+  startGame() {
+    // creat the layout (instantiation and rendering of each card instance. (with elements))
     const newLayout = new Layout(3, 3)
     newLayout.renderLayout()
-    
+
     while( this.remainingCards > 1 ) {
 
     }
   }
 
   gameover() {
+    // This is what is called everytime we call click on a card
     document.getElementById('game-over-text').className('visible');
+    // call to resetGame()
+  }
+
+  resetGame() {
+    //  reset variables here
+    // change class of popupwindow UI (with modal)
   }
 
 
